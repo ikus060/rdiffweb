7f82ee41e39a635fa03beab973d1f92818cfbf36
Patrik Dufresne <ikus060@gmail.com>
2012-10-27 23:04:57 -0400

    Use overlay to select revision to be restored.

--- rdiffWeb/static/script.js
+++ rdiffWeb/static/script.js
@@ -1,36 +1,3 @@
-// menubar
-$(document).ready(function() {
-	var closing = undefined;
-	var closingInterval = undefined;
-	function doClose()
-	{
-	    $(closing).children("ul").stop(true, true).slideUp(10);
-	    closing = undefined;
-	    closingInterval = undefined;
-	}
-	
-	$(".revision").children("ul").hide();
-	$(".revision").hover(function() {
-	    if (closing)
-	    {
-	        clearTimeout(closingInterval);
-	        if (closing != this)
-	        {
-	            doClose();
-	        }
-	        else
-	        {
-	            closing = undefined;
-	            closingInterval = undefined;
-	        }
-	    }
-	    $(this).children("ul").stop(true, true).slideDown(10);
-	}, function() {
-	    closing = this;
-	    closingInterval = setTimeout(doClose, 100); 
-	});
-});
-
 // Tooltip
 $(document).ready(function() {
 	$("[title]").tooltip({ position: "bottom center", relative:true});
@@ -46,5 +13,11 @@ $(document).ready(function() {
 
 // Overlay
 $(document).ready(function() {
-	$(".overlay-link[rel]").overlay({fixed:false,top:"center",left:"center"});
+	$(".overlay-link[rel]").each(function(){
+		targetId = $(this).attr('rel');
+		$(this).removeAttr('rel');
+		targetId = targetId.replace(/#/g,'');
+		targetEle = document.getElementById(targetId);
+		$(this).overlay({fixed:false,top:"center",left:"center",target:targetEle});
+	}) 
 });
