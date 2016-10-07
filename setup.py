db7585a6818888371e026a097b12cdb68ebed2d2
Patrik Dufresne <ikus060@gmail.com>
2016-10-07 12:19:12 -0400

    Remove filltmpl from default build.

--- setup.py
+++ setup.py
@@ -206,7 +206,6 @@ class build(build_):
 
     sub_commands = build_.sub_commands[:]
     sub_commands.insert(0, ('compile_all_catalogs', None))
-    sub_commands.insert(0, ('filltmpl', None))
 
 
 class tox(TestCommand):
