c2eff77edfde5b301d103db4d349c678e671de19
Jenkins <jenkins@icefyre>
2016-10-11 07:52:56 -0400

    Release 0.9.4

--- setup.py
+++ setup.py
@@ -242,7 +242,7 @@ if PY2:
 
 setup(
     name='rdiffweb',
-    version='0.9.4.dev1',
+    version='0.9.4',
     description='A web interface to rdiff-backup repositories',
     author='Patrik Dufresne',
     author_email='info@patrikdufresne.com',
