4b85b12ddb1247a281e47a9289e87f37bd13728c
Jenkins <jenkins@icefyre>
2017-05-25 18:11:09 -0400

    Prepare next development version

--- setup.py
+++ setup.py
@@ -242,7 +242,7 @@ if PY2:
 
 setup(
     name='rdiffweb',
-    version='0.10.2',
+    version='0.10.3.dev1',
     description='A web interface to rdiff-backup repositories',
     author='Patrik Dufresne',
     author_email='info@patrikdufresne.com',
