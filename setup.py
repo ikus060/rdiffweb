1bc0f01f4a45eafe0662d96125bd14c8560e8549
Patrik Dufresne <info@patrikdufresne.com>
2020-04-22 20:26:35 -0400

    Bump to v1.3.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         "setuptools_scm",
     ],
     install_requires=[
-        "rdiffweb==1.3.1",
+        "rdiffweb==1.3.2",
         "cherrypy>=16.0.0",
         "requests",
     ],
