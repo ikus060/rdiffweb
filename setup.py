e10291ba76af096a726197ce7d3880c091753ccc
Patrik Dufresne <info@patrikdufresne.com>
2021-04-01 08:02:55 -0400

    Bump rdiffweb to 2.1.1.dev78+g34f1634 with background job scheduler

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
         "setuptools_scm",
     ],
     install_requires=[
-        "rdiffweb==2.1.1.dev42+gff7e160",
+        "rdiffweb==2.1.1.dev78+g34f1634",
         "cherrypy>=16.0.0",
         "requests",
         snakeoil_ver,
