461e92af9b4f187e39ec06c6aaf4034972f1a9fa
IKUS Soft robot <robot@ikus-soft.com>
2022-03-13 23:41:14 +0000

    Bump rdiffweb to v2.4.0a1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
         "setuptools_scm>=5.0.1",
     ],
     install_requires=[
-        "rdiffweb==2.3.9",
+        "rdiffweb==2.4.0a1",
         "cherrypy>=16.0.0",
         "requests",
         "tzlocal~=2.0",
