5d441be04eef43750fa6c690cb135358af427ad0
Patrik Dufresne <info@patrikdufresne.com>
2022-06-21 13:44:45 -0400

    Bump rdiffweb to 2.4.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
         "setuptools_scm>=5.0.1",
     ],
     install_requires=[
-        "rdiffweb==2.4.0.a8",
+        "rdiffweb==2.4.0",
         "cherrypy>=18.0.0",
         "requests",
         "tzlocal~=2.0",
