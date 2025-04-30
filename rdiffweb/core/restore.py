715fbad66c5a34c0a535af17d632d1021a0fe1a7
Patrik Dufresne <patrik@ikus-soft.com>
2025-04-30 11:40:50 -0400

    File header and license cleanups
    Signed-off-by: Patrik Dufresne <patrik@ikus-soft.com>

--- rdiffweb/core/restore.py
+++ rdiffweb/core/restore.py
@@ -12,7 +12,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 Module is responsible to restore rdiff-backup repository into an archive in a
 transparent way to restore and archive at the same time allowing creation of the
